34ef954f1b02f701099ec51bf61fe3e4f8b5acb4
Vitor Vilete <vitorgarcesvilete@gmail.com>
2024-01-27 16:12:03 -0300

    Adding animations to prank 3

--- Assets/Scripts/Visual/InterviewPlayerVisual.cs
+++ Assets/Scripts/Visual/InterviewPlayerVisual.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPersonVisual : MonoBehaviour
+public class InterviewPlayerVisual : MonoBehaviour
 {
     private bool isPlaying = false;
     private const string IS_PLAYING = "IS_PLAYING";
-    [SerializeField] Animator playerPersonAnimator;
+    [SerializeField] Animator interviewPlayerAnimator;
 
     void Start()
     {
-        MinigameManager.Instance.OnStateChanged += MinigameManager_OnStateChanged;        
+        MinigameManager.Instance.OnStateChanged += MinigameManager_OnStateChanged;
     }
 
     private void MinigameManager_OnStateChanged(object sender, System.EventArgs e)
@@ -23,6 +23,6 @@ public class PlayerPersonVisual : MonoBehaviour
         {
             isPlaying = false;
         }
-        playerPersonAnimator.SetBool(IS_PLAYING, isPlaying);
+        interviewPlayerAnimator.SetBool(IS_PLAYING, isPlaying);
     }
 }
--- Assets/Scripts/Visual/InterviewedPersonVisual.cs
+++ Assets/Scripts/Visual/InterviewedPersonVisual.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPersonVisual : MonoBehaviour
+public class InterviewedPersonVisual : MonoBehaviour
 {
     private bool isPlaying = false;
     private const string IS_PLAYING = "IS_PLAYING";
-    [SerializeField] Animator playerPersonAnimator;
+    [SerializeField] Animator interviewedPersonAnimator;
 
     void Start()
     {
-        MinigameManager.Instance.OnStateChanged += MinigameManager_OnStateChanged;        
+        MinigameManager.Instance.OnStateChanged += MinigameManager_OnStateChanged;
     }
 
     private void MinigameManager_OnStateChanged(object sender, System.EventArgs e)
@@ -23,6 +23,6 @@ public class PlayerPersonVisual : MonoBehaviour
         {
             isPlaying = false;
         }
-        playerPersonAnimator.SetBool(IS_PLAYING, isPlaying);
+        interviewedPersonAnimator.SetBool(IS_PLAYING, isPlaying);
     }
 }
